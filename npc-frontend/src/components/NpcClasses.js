b796812b47a63b228c71d635aea1fabe14dd5cc0
Dominic Barajas <dominicbarajas548@gmail.com>
2021-08-29 13:37:54 -0600

    update race page and class page
    add all data needed to show stats for the classes in the app. and add the titles for racial traits to race page for each race.

--- npc-frontend/src/components/NpcClasses.js
+++ npc-frontend/src/components/NpcClasses.js
@@ -32,7 +32,7 @@ export const NpcClasses = () => {
                 <strong>Hit Die:</strong>
                 d8<br />
                 <strong>Primary Ability:</strong>
-                Wisdom
+                Wisdom<br />
                 <strong>Saves:</strong>
                 Wisdom & Charisma<br />
             </p>
@@ -40,71 +40,101 @@ export const NpcClasses = () => {
             <p>A priest of the Old Faith, wielding the powers of nature and adopting animal forms</p>
             <p>
                 <strong>Hit Die:</strong>
+                d8<br />
                 <strong>Primary Ability:</strong>
+                Wisdom<br />
                 <strong>Saves:</strong>
+                Intelligence & Wisdom<br />
             </p>
             <h2>Fighter</h2>
             <p>A master of martial combat, skilled with a variety of weapons and armor</p>
             <p>
                 <strong>Hit Die:</strong>
+                d10<br />
                 <strong>Primary Ability:</strong>
+                Strength & Dexterity<br />
                 <strong>Saves:</strong>
+                Strength<br />
             </p>
             <h2>Monk</h2>
             <p>A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection</p>
             <p>
                 <strong>Hit Die:</strong>
+                d8<br />
                 <strong>Primary Ability:</strong>
+                Dexterity & Wisdom<br />
                 <strong>Saves:</strong>
+                Strength & Dexterity
             </p>
             <h2>Paladin</h2>
             <p>A holy warrior bound to a sacred oath</p>
             <p>
                 <strong>Hit Die:</strong>
+                d10<br />
                 <strong>Primary Ability:</strong>
+                Strenght & Charisma<br />
                 <strong>Saves:</strong>
+                Wisdom & Charisma<br />
             </p>
             <h2>Ranger</h2>
             <p>A warrior who combats threats on the edges of civilization</p>
             <p>
                 <strong>Hit Die:</strong>
+                d10<br />
                 <strong>Primary Ability:</strong>
+                Dexterity & Wisdom<br />
                 <strong>Saves:</strong>
+                Strength & Dexterity
             </p>
             <h2>Rogue</h2>
             <p>A scoundrel who uses stealth and trickery to overcome obstacles and enemies</p>
             <p>
                 <strong>Hit Die:</strong>
+                d8<br />
                 <strong>Primary Ability:</strong>
+                Dexterity<br />
                 <strong>Saves:</strong>
+                Dexterity & Intelligence
             </p>
             <h2>Sorcerer</h2>
             <p>A spellcaster who draws on inherent magic from a gift or bloodline</p>
             <p>
                 <strong>Hit Die:</strong>
+                d6<br />
                 <strong>Primary Ability:</strong>
+                Charisma<br />
                 <strong>Saves:</strong>
+                Constitution & Charisma
             </p>
             <h2>Warlock</h2>
             <p>A wielder of magic that is derived from a bargain with an extraplanar entity</p>
             <p>
                 <strong>Hit Die:</strong>
+                d8<br />
                 <strong>Primary Ability:</strong>
+                Charisma<br />
                 <strong>Saves:</strong>
+                Wisdom & Charisma
             </p>
             <h2>Wizard</h2>
             <p>A scholarly magic-user capable of manipulating the structures of reality</p>
             <p>
                 <strong>Hit Die:</strong>
+                d6<br />
                 <strong>Primary Ability:</strong>
+                Intelligence<br />
                 <strong>Saves:</strong>
+                Intelligence & Wisdom
             </p>
             <h2>Artificer</h2>
             <p>Masters of invention, artificers use ingenuity and magic to unlock extraordinary capabilities in objects.</p>
             <p>
                 <strong>Hit Die:</strong>
+                d8<br />
                 <strong>Primary Ability:</strong>
+                Intelligence<br />
                 <strong>Saves:</strong>
+                Constitution & Intelligence
             </p>
         </div>
     )

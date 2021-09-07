ea11b00a4413e8d25898fc334ec843bb04e51cfc
Dominic Barajas <dominicbarajas548@gmail.com>
2021-09-07 15:27:39 -0600

    move array info
    move background array, armor array, title array into assets folder

---
+++ npc-frontend/src/assets/TitleArray.js
@@ -0,0 +1,30 @@
+export const FighterTitleArray = ["Veteran", "Warrior", "Swordsman", "Sword Master", "Hero", "Swashbuckler", "Myrmidon", "Champion", "Hero", "Superhero", "Lord", "Lord, 10th", "Mercenary", "Soldier", "Sellsword", "Gladiator", "Conqueror", "Dominator"]
+export const WizardTitleArray = [ "Hedge Wizard", "Apprentice", "Scholar", "Journeymage", "Curate", "Scrollmaster", "Sage", "Visionary", "Chronicler", "Loremaster"]
+export const SorcererTitleArray = [ "Cantrap", "Magician", "Metamage", "Karmabender", "Spelltwister", "Magus", "Wildblood", "Spellslinger", "Sanguiner", "High Magus"]
+export const WarlockTitleArray = [ "Neophyte", "Invoker", "Caller", "Hexer", "Niximancer", "Witch", "Thaumaturgist", "Haunt", "Dreadbirther", "Chained One"]
+export const ClericTitleArray = [ "Aspirant", "Acolyte", "Adept", "Missionary", "Apostle", "Preacher", "Priest", "Abbot", "Bishop", "High Priest"]	
+export const RogueTitleArray = [ "Footpad", "Pickpocket", "Rutterkin", "Robber", "Viper", "Trickster", "Burglar", "Spy", "Creeper", "Adder", "Apprentice", "Pilferer", "Cutpurse", "Master Pilferer", "Sharper", "Thief", "Master Thief", "Disciple", "Initiate", "Killer", "Murderer", "Slayer", "Cutthroat", "Dacoit", "Thug", "Executioner", "Assassin", "Senior Assassin", "Prime Assassin", "Guildmaster of Assassins", "Chief Assassin", "Expert Assassin", "Apprentice", "Swashbuckler"]
+export const DruidTitleArray = [ "Initiate", "Devotee", "Beastling", "Dreamer", "Shifter", "Herald", "Grovelurker", "Shaper", "Springwalker", "Elder Druid"]
+export const MonkTitleArray = [ "Novice", "Grand Master of East Winds", "Grand Master of Winter", "Grand Master of Summer", "Grand Master of Spring", "Grand Master of Flower", "Grand Master of Autumn", "Grand Master of South Winds", "Grand Master of West Winds", "Grand Master of North Winds", "Grand Master of Dragons", "Grand Master", "Master", "Immaculate"]
+export const PaladinTitleArray = [ "Gallant", "Squire", "Oathbound", "Keeper", "Sentinel", "Protector", "Inquisitor", "Bulwark", "Justiciar", "Legend"]
+export const RangerTitleArray = [ "Runner", "Ranger Lord, 11th", "Scout", "Courser", "Ranger Lord", "Ranger", "Tracker", "Guide", "Ranger Knight", "Wanderer", "Wildling", "Hunter", "Sojourner", "Wayfarer", "Preserver", "Strider", "Naturekin", "Treewarden", "Pathfinder"]
+export const BardTitleArray = [ "Crier", "Accompanist", "Storyteller", "Rhymer", "Poet", "Singer", "Minstrel", "Balladeer", "Songsmith", "Troubadour"]
+export const ArtificerTitleArray = [ "Alchemist", "Armorer", "Artillerist", "Battle Smith"]
+export const BarbarianTitleArray = [ "Thug", "Brute", "Mauler", "Aggressor", "Plunderer", "Bloodrager", "Slaughterer", "Rampager", "Bloodletter", "Destroyer"]
+
+    
+    export default { 
+        FighterTitleArray,
+        WizardTitleArray,
+        SorcererTitleArray,
+        WarlockTitleArray,
+        ClericTitleArray,
+        RogueTitleArray,
+        DruidTitleArray,
+        MonkTitleArray,
+        PaladinTitleArray,
+        RangerTitleArray,
+        BardTitleArray,
+        ArtificerTitleArray,
+        BarbarianTitleArray
+    }

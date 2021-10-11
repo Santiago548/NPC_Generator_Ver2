0c7243b4b88d967e268c4f5e73ded65657cc3b1d
Dominic Barajas <dominicbarajas548@gmail.com>
2021-10-11 14:12:00 -0600

    add stat container
    add images for stat containers and create css to center text within the stat boxes

--- npc-frontend/src/components/npcFullCard.js
+++ npc-frontend/src/components/npcFullCard.js
@@ -1,4 +1,10 @@
 import React from "react";
+import STR from "../assets/images/strlogo.png"
+import DEX from "../assets/images/dexlogo.png"
+import CON from "../assets/images/conlogo.png"
+import INT from "../assets/images/intlogo.png"
+import WIS from "../assets/images/wislogo.png"
+import CHA from "../assets/images/chalogo.png"
 
 const NpcFullCard = ({ npc, deleteNpc }) => (
   <div>
@@ -11,17 +17,17 @@ const NpcFullCard = ({ npc, deleteNpc }) => (
           <h3>
             {npc.firstName} {npc.lastName}
           </h3>
-          <br />
-          The {npc.alignment} 
+
+          The {npc.alignment}
           < br />
           {npc.race} {npc.npcClass} {npc.title}
         </div>
         <br />
         <fieldset className='traits'>
-        Traits: {npc.trait}<br />
-        Quirk 1: {npc.quirk1}<br />
-        Quirk 2:{npc.quirk2}<br />
-        Physical Trait: {npc.notablePhysicalFeature}
+          Traits: {npc.trait}<br />
+          Quirk 1: {npc.quirk1}<br />
+          Quirk 2:{npc.quirk2}<br />
+          Physical Trait: {npc.notablePhysicalFeature}
         </fieldset>
         <br />
         HEALTH: <span>{npc.health + npc.con * 6} HP</span>
@@ -38,12 +44,42 @@ const NpcFullCard = ({ npc, deleteNpc }) => (
         <div>
           STR: | DEX: | CON: | INT: | WIS: | CHA:
           <div>
-            +{npc.str} /  {(10 + npc.str * 2)} ||
-            +{npc.dex} /  {(10 + npc.dex * 2)} ||
-            +{npc.con} /  {(10 + npc.con * 2)} ||
-            +{npc.int} /  {(10 + npc.int * 2)} ||
-            +{npc.wis} /  {(10 + npc.wis * 2)} ||
-            +{npc.cha} /  {(10 + npc.cha * 2)} ||
+            <div className="strContainer">
+              <img className="strImage" src={STR} alt="strContainer" />
+              <div className="strStat">
+                +{npc.str} /  {(10 + npc.str * 2)}
+              </div>
+            </div>
+            <div className="dexContainer">
+              <img className="dexImage" src={DEX} alt="dexContainer" />
+              <div className="dexStat">
+                +{npc.dex} /  {(10 + npc.dex * 2)}
+              </div>
+            </div>
+            <div className="conContainer">
+              <img className="conImage" src={CON} alt="conContainer" />
+              <div className="conStat">
+                +{npc.con} /  {(10 + npc.con * 2)}
+              </div>
+            </div>
+            <div className="intContainer">
+              <img className="intImage" src={INT} alt="intContainer" />
+              <div className="intStat">
+                +{npc.int} /  {(10 + npc.int * 2)}
+              </div>
+            </div>
+            <div className="wisContainer">
+              <img className="wisImage" src={WIS} alt="wisContainer" />
+              <div className="wisStat">
+                +{npc.wis} /  {(10 + npc.wis * 2)}
+              </div>
+            </div>
+            <div className="chaContainer">
+              <img className="chaImage" src={CHA} alt="chaContainer" />
+              <div className="chaStat">
+                +{npc.cha} /  {(10 + npc.cha * 2)}
+              </div>
+            </div>
           </div>
         </div>
         Melee: {npc.melee} <br />
@@ -51,9 +87,11 @@ const NpcFullCard = ({ npc, deleteNpc }) => (
         <br />
       </div>
       Background:
-      <fieldset>{npc.background}</fieldset>
+      <fieldset>
+        {npc.background}
+      </fieldset>
       <br />
-      
+
     </fieldset>
   </div>
 );

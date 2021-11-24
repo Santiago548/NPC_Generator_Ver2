4a84d63175492523f63882210876c8657a61b112
Dominic Barajas <dominicbarajas548@gmail.com>
2021-11-24 10:23:38 -0700

    update css to have classes adjust based on the size of the screen

--- npc-frontend/src/components/NpcClasses.js
+++ npc-frontend/src/components/NpcClasses.js
@@ -5,240 +5,241 @@ export const NpcClasses = () => {
     <div>
       <h1>Classes</h1>
 
-
-    <fieldset className="classesContainer">
-
-      <fieldset className="leftClassDescription">
-        <h2>Barbarian</h2>
-        <p>
-          A fierce warrior of primitive background who can enter a battle rage
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d12
-          <br />
-          <strong>Primary Ability: </strong>
-          Strength
-          <br />
-          <strong>Saves: </strong>
-          Strength & Constitution
-          <br />
-        </p>
-      </fieldset>
-
-      <fieldset className="rightClassDescription">
-        <h2>Bard</h2>
-        <p>An inspiring magician whose power echoes the music of creation</p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Charisma
-          <br />
-          <strong>Saves: </strong>
-          Dexterity & Charisma
-          <br />
-        </p>
-      </fieldset>
-
-</fieldset>
-
-<fieldset className="classesContainer">
-        
-      <fieldset className="leftClassDescription">
-        <h2>Cleric</h2>
-        <p>
-          A priestly champion who wields divine magic in service of a higher
-          power
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Wisdom
-          <br />
-          <strong>Saves: </strong>
-          Wisdom & Charisma
-          <br />
-        </p>
-      </fieldset>
-
-
-      <fieldset className="rightClassDescription">
-        <h2>Druid</h2>
-        <p>
-          A priest of the Old Faith, wielding the powers of nature and adopting
-          animal forms
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Wisdom
-          <br />
-          <strong>Saves: </strong>
-          Intelligence & Wisdom
-          <br />
-        </p>
-      </fieldset>
-</fieldset>
-
-<fieldset className="classesContainer">
-        
+      <fieldset className="classesContainer">
         <fieldset className="leftClassDescription">
-        <h2>Fighter</h2>
-        <p>
-          A master of martial combat, skilled with a variety of weapons and
-          armor
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d10
-          <br />
-          <strong>Primary Ability: </strong>
-          Strength & Dexterity
-          <br />
-          <strong>Saves: </strong>
-          Strength
-          <br />
-        </p>
+          <h2>Barbarian</h2>
+          <hr />
+          <p>
+            A fierce warrior of primitive background who can enter a battle rage
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d12
+            <br />
+            <strong>Primary Ability: </strong>
+            Strength
+            <br />
+            <strong>Saves: </strong>
+            Strength & Constitution
+            <br />
+          </p>
+        </fieldset>
+
+        <fieldset className="rightClassDescription">
+          <h2>Bard</h2>
+          <hr />
+          <p>An inspiring magician whose power echoes the music of creation</p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Charisma
+            <br />
+            <strong>Saves: </strong>
+            Dexterity & Charisma
+            <br />
+          </p>
+        </fieldset>
       </fieldset>
 
-
-      <fieldset className="rightClassDescription">
-        <h2>Monk</h2>
-        <p>
-          A master of martial arts, harnessing the power of the body in pursuit
-          of physical and spiritual perfection
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Dexterity & Wisdom
-          <br />
-          <strong>Saves: </strong>
-          Strength & Dexterity
-        </p>
-      </fieldset>
-</fieldset>
-
-<fieldset className="classesContainer">
-        
+      <fieldset className="classesContainer">
         <fieldset className="leftClassDescription">
-        <h2>Paladin</h2>
-        <p>A holy warrior bound to a sacred oath</p>
-        <p>
-          <strong>Hit Die: </strong>
-          d10
-          <br />
-          <strong>Primary Ability: </strong>
-          Strenght & Charisma
-          <br />
-          <strong>Saves: </strong>
-          Wisdom & Charisma
-          <br />
-        </p>
+          <h2>Cleric</h2>
+          <hr />
+          <p>
+            A priestly champion who wields divine magic in service of a higher
+            power
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Wisdom
+            <br />
+            <strong>Saves: </strong>
+            Wisdom & Charisma
+            <br />
+          </p>
+        </fieldset>
+
+        <fieldset className="rightClassDescription">
+          <h2>Druid</h2>
+          <hr />
+          <p>
+            A priest of the Old Faith, wielding the powers of nature and
+            adopting animal forms
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Wisdom
+            <br />
+            <strong>Saves: </strong>
+            Intelligence & Wisdom
+            <br />
+          </p>
+        </fieldset>
       </fieldset>
 
-
-      <fieldset classname="rightClassDescription">
-        <h2>Ranger</h2>
-        <p>A warrior who combats threats on the edges of civilization</p>
-        <p>
-          <strong>Hit Die: </strong>
-          d10
-          <br />
-          <strong>Primary Ability: </strong>
-          Dexterity & Wisdom
-          <br />
-          <strong>Saves: </strong>
-          Strength & Dexterity
-        </p>
-      </fieldset>
-</fieldset>
-
-<fieldset className="classesContainer">
-        
+      <fieldset className="classesContainer">
         <fieldset className="leftClassDescription">
-        <h2>Rogue</h2>
-        <p>
-          A scoundrel who uses stealth and trickery to overcome obstacles and
-          enemies
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Dexterity
-          <br />
-          <strong>Saves: </strong>
-          Dexterity & Intelligence
-        </p>
+          <h2>Fighter</h2>
+          <hr />
+          <p>
+            A master of martial combat, skilled with a variety of weapons and
+            armor
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d10
+            <br />
+            <strong>Primary Ability: </strong>
+            Strength & Dexterity
+            <br />
+            <strong>Saves: </strong>
+            Strength
+            <br />
+          </p>
+        </fieldset>
+
+        <fieldset className="rightClassDescription">
+          <h2>Monk</h2>
+          <hr />
+          <p>
+            A master of martial arts, harnessing the power of the body in
+            pursuit of physical and spiritual perfection
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Dexterity & Wisdom
+            <br />
+            <strong>Saves: </strong>
+            Strength & Dexterity
+          </p>
+        </fieldset>
       </fieldset>
 
-      <fieldset className="rigthClassDescription">
-        <h2>Sorcerer</h2>
-        <p>
-          A spellcaster who draws on inherent magic from a gift or bloodline
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d6
-          <br />
-          <strong>Primary Ability: </strong>
-          Charisma
-          <br />
-          <strong>Saves: </strong>
-          Constitution & Charisma
-        </p>
+      <fieldset className="classesContainer">
+        <fieldset className="leftClassDescription">
+          <h2>Paladin</h2>
+          <hr />
+          <p>A holy warrior bound to a sacred oath</p>
+          <p>
+            <strong>Hit Die: </strong>
+            d10
+            <br />
+            <strong>Primary Ability: </strong>
+            Strenght & Charisma
+            <br />
+            <strong>Saves: </strong>
+            Wisdom & Charisma
+            <br />
+          </p>
+        </fieldset>
+        <fieldset className="rightClassDescription">
+          <h2>Ranger</h2>
+          <hr />
+          <p>A warrior who combats threats on the edges of civilization</p>
+          <p>
+            <strong>Hit Die: </strong>
+            d10
+            <br />
+            <strong>Primary Ability: </strong>
+            Dexterity & Wisdom
+            <br />
+            <strong>Saves: </strong>
+            Strength & Dexterity
+          </p>
+        </fieldset>
       </fieldset>
-</fieldset>
 
-<fieldset className="classesContainer">
-        
-      <fieldset className="leftClassDescription">
-        <h2>Warlock</h2>
-        <p>
-          A wielder of magic that is derived from a bargain with an extraplanar
-          entity
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d8
-          <br />
-          <strong>Primary Ability: </strong>
-          Charisma
-          <br />
-          <strong>Saves: </strong>
-          Wisdom & Charisma
-        </p>
+      <fieldset className="classesContainer">
+        <fieldset className="leftClassDescription">
+          <h2>Rogue</h2>
+          <hr />
+          <p>
+            A scoundrel who uses stealth and trickery to overcome obstacles and
+            enemies
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Dexterity
+            <br />
+            <strong>Saves: </strong>
+            Dexterity & Intelligence
+          </p>
+        </fieldset>
+
+        <fieldset className="rightClassDescription">
+          <h2>Sorcerer</h2>
+          <hr />
+          <p>
+            A spellcaster who draws on inherent magic from a gift or bloodline
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d6
+            <br />
+            <strong>Primary Ability: </strong>
+            Charisma
+            <br />
+            <strong>Saves: </strong>
+            Constitution & Charisma
+          </p>
+        </fieldset>
       </fieldset>
-      <fieldset className="rightClassDescription">
-        <h2>Wizard</h2>
-        <p>
-          A scholarly magic-user capable of manipulating the structures of
-          reality
-        </p>
-        <p>
-          <strong>Hit Die: </strong>
-          d6
-          <br />
-          <strong>Primary Ability: </strong>
-          Intelligence
-          <br />
-          <strong>Saves: </strong>
-          Intelligence & Wisdom
-        </p>
-      </fieldset>
+
+      <fieldset className="classesContainer">
+        <fieldset className="leftClassDescription">
+          <h2>Warlock</h2>
+          <hr />
+          <p>
+            A wielder of magic that is derived from a bargain with an
+            extraplanar entity
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d8
+            <br />
+            <strong>Primary Ability: </strong>
+            Charisma
+            <br />
+            <strong>Saves: </strong>
+            Wisdom & Charisma
+          </p>
+        </fieldset>
+        <fieldset className="rightClassDescription">
+          <h2>Wizard</h2>
+          <hr />
+          <p>
+            A scholarly magic-user capable of manipulating the structures of
+            reality
+          </p>
+          <p>
+            <strong>Hit Die: </strong>
+            d6
+            <br />
+            <strong>Primary Ability: </strong>
+            Intelligence
+            <br />
+            <strong>Saves: </strong>
+            Intelligence & Wisdom
+          </p>
+        </fieldset>
       </fieldset>
       <fieldset>
         <h2>Artificer</h2>
+        <hr />
         <p>
           Masters of invention, artificers use ingenuity and magic to unlock
           extraordinary capabilities in objects.

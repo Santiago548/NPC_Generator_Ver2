958d21d640dc9ebd969147ce435f8cbaffe42330
Dominic Barajas <dominicbarajas548@gmail.com>
2021-11-24 10:38:20 -0700

    update race page with css to create two columns

--- npc-frontend/src/components/NpcRaces.js
+++ npc-frontend/src/components/NpcRaces.js
@@ -1,88 +1,169 @@
-import React from 'react'
+import React from "react";
 
 export const NpcRaces = () => {
-    return (
-        <div>
-            <h1>Races</h1>
-            <fieldset>
-            <h2>Dragonborn</h2>
-            <p>Dragonborn look very much like dragons standing erect in humanoid form, though they lack wings or a tail.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Dwarf</h2>
-            <p>Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Elf</h2>
-            <p>Elves are a magical people of otherworldly grace, living in the world but not entirely part of it.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Gnome</h2>
-            <p>A gnome’s energy and enthusiasm for living shines through every inch of his or her tiny body.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Half-Elf</h2>
-            <p>Half-elves combine what some say are the best qualities of their elf and human parents.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Halfling</h2>
-            <p>The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Human</h2>
-            <p>Humans are the most adaptable and ambitious people among the common races. Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Half-Orc</h2>
-            <p>Half-orcs’ grayish pigmentation, sloping foreheads, jutting jaws, prominent teeth, and towering builds make their orcish heritage plain for all to see.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Tiefling</h2>
-            <p>To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Goliath</h2>
-            <p>Strong and reclusive, every day brings a new challenge to a goliath.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Firbolg</h2>
-            <p>Firbolg tribes cloister in remote forest strongholds, preferring to spend their days in quiet harmony with the woods.</p>
-            <strong>Racial Traits</strong>   
-            </fieldset>
-            <fieldset>
-            <h2>Goblin</h2>
-            <p>Goblins occupy an uneasy place in a dangerous world, and they react by lashing out at any creatures they believe they can bully.</p>
-            <strong>Racial Traits</strong>    
-            </fieldset>
-            <fieldset>
-            <h2>Orc</h2>
-            <p>Orcs live a life that has no place for weakness, and every warrior must be strong enough to take what is needed by force.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset>
-            <h2>Tabaxi</h2>
-            <p>Hailing from a strange and distant land, wandering tabaxi are catlike humanoids driven by curiosity to collect interesting artifacts, gather tales and stories, and lay eyes on all the world’s wonders.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-            <fieldset> 
-            <h2>Warforged</h2>
-            <p>Warforged are made from wood and metal, but they can feel pain and emotion. Built as weapons, they must now find a purpose beyond war.</p>
-            <strong>Racial Traits</strong>
-            </fieldset>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Races</h1>
 
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Dragonborn</h2>
+          <hr />
+          <p>
+            Dragonborn look very much like dragons standing erect in humanoid
+            form, though they lack wings or a tail.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Dwarf</h2>
+          <hr />
+          <p>
+            Bold and hardy, dwarves are known as skilled warriors, miners, and
+            workers of stone and metal.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
 
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Elf</h2>
+          <hr />
+          <p>
+            Elves are a magical people of otherworldly grace, living in the
+            world but not entirely part of it.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Gnome</h2>
+          <hr />
+          <p>
+            A gnome’s energy and enthusiasm for living shines through every inch
+            of his or her tiny body.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
 
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Half-Elf</h2>
+          <hr />
+          <p>
+            Half-elves combine what some say are the best qualities of their elf
+            and human parents.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Halfling</h2>
+          <hr />
+          <p>
+            The diminutive halflings survive in a world full of larger creatures
+            by avoiding notice or, barring that, avoiding offense.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
 
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Human</h2>
+          <hr />
+          <p>
+            Humans are the most adaptable and ambitious people among the common
+            races. Whatever drives them, humans are the innovators, the
+            achievers, and the pioneers of the worlds.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Half-Orc</h2>
+          <hr />
+          <p>
+            Half-orcs’ grayish pigmentation, sloping foreheads, jutting jaws,
+            prominent teeth, and towering builds make their orcish heritage
+            plain for all to see.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
+
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Tiefling</h2>
+          <hr />
+          <p>
+            To be greeted with stares and whispers, to suffer violence and
+            insult on the street, to see mistrust and fear in every eye: this is
+            the lot of the tiefling.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Goliath</h2>
+          <hr />
+          <p>
+            Strong and reclusive, every day brings a new challenge to a goliath.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
+
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Firbolg</h2>
+          <hr />
+          <p>
+            Firbolg tribes cloister in remote forest strongholds, preferring to
+            spend their days in quiet harmony with the woods.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Goblin</h2>
+          <hr />
+          <p>
+            Goblins occupy an uneasy place in a dangerous world, and they react
+            by lashing out at any creatures they believe they can bully.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
+
+      <fieldset className="raceContainer">
+        <fieldset className="leftRaceDescription">
+          <h2>Orc</h2>
+          <hr />
+          <p>
+            Orcs live a life that has no place for weakness, and every warrior
+            must be strong enough to take what is needed by force.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+        <fieldset className="rightRaceDescription">
+          <h2>Tabaxi</h2>
+          <hr />
+          <p>
+            Hailing from a strange and distant land, wandering tabaxi are
+            catlike humanoids driven by curiosity to collect interesting
+            artifacts, gather tales and stories, and lay eyes on all the world’s
+            wonders.
+          </p>
+          <strong>Racial Traits</strong>
+        </fieldset>
+      </fieldset>
+      <fieldset>
+        <h2>Warforged</h2>
+        <hr />
+        <p>
+          Warforged are made from wood and metal, but they can feel pain and
+          emotion. Built as weapons, they must now find a purpose beyond war.
+        </p>
+        <strong>Racial Traits</strong>
+      </fieldset>
+    </div>
+  );
+};

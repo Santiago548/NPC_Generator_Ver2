a038a8a7ad7eb340e9f5c3964a2076baa11b1fba
Dominic Barajas <dominicbarajas548@gmail.com>
2021-11-24 10:13:15 -0700

    update CSS for classes page to show classes side by side in fieldsets

--- npc-frontend/src/components/NpcClasses.js
+++ npc-frontend/src/components/NpcClasses.js
@@ -4,7 +4,11 @@ export const NpcClasses = () => {
   return (
     <div>
       <h1>Classes</h1>
-      <fieldset>
+
+
+    <fieldset className="classesContainer">
+
+      <fieldset className="leftClassDescription">
         <h2>Barbarian</h2>
         <p>
           A fierce warrior of primitive background who can enter a battle rage
@@ -21,7 +25,8 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+
+      <fieldset className="rightClassDescription">
         <h2>Bard</h2>
         <p>An inspiring magician whose power echoes the music of creation</p>
         <p>
@@ -36,7 +41,12 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+
+</fieldset>
+
+<fieldset className="classesContainer">
+        
+      <fieldset className="leftClassDescription">
         <h2>Cleric</h2>
         <p>
           A priestly champion who wields divine magic in service of a higher
@@ -54,7 +64,9 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+
+
+      <fieldset className="rightClassDescription">
         <h2>Druid</h2>
         <p>
           A priest of the Old Faith, wielding the powers of nature and adopting
@@ -72,7 +84,11 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+</fieldset>
+
+<fieldset className="classesContainer">
+        
+        <fieldset className="leftClassDescription">
         <h2>Fighter</h2>
         <p>
           A master of martial combat, skilled with a variety of weapons and
@@ -90,7 +106,9 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+
+
+      <fieldset className="rightClassDescription">
         <h2>Monk</h2>
         <p>
           A master of martial arts, harnessing the power of the body in pursuit
@@ -107,7 +125,11 @@ export const NpcClasses = () => {
           Strength & Dexterity
         </p>
       </fieldset>
-      <fieldset>
+</fieldset>
+
+<fieldset className="classesContainer">
+        
+        <fieldset className="leftClassDescription">
         <h2>Paladin</h2>
         <p>A holy warrior bound to a sacred oath</p>
         <p>
@@ -122,7 +144,9 @@ export const NpcClasses = () => {
           <br />
         </p>
       </fieldset>
-      <fieldset>
+
+
+      <fieldset classname="rightClassDescription">
         <h2>Ranger</h2>
         <p>A warrior who combats threats on the edges of civilization</p>
         <p>
@@ -136,7 +160,11 @@ export const NpcClasses = () => {
           Strength & Dexterity
         </p>
       </fieldset>
-      <fieldset>
+</fieldset>
+
+<fieldset className="classesContainer">
+        
+        <fieldset className="leftClassDescription">
         <h2>Rogue</h2>
         <p>
           A scoundrel who uses stealth and trickery to overcome obstacles and
@@ -153,7 +181,8 @@ export const NpcClasses = () => {
           Dexterity & Intelligence
         </p>
       </fieldset>
-      <fieldset>
+
+      <fieldset className="rigthClassDescription">
         <h2>Sorcerer</h2>
         <p>
           A spellcaster who draws on inherent magic from a gift or bloodline
@@ -169,7 +198,11 @@ export const NpcClasses = () => {
           Constitution & Charisma
         </p>
       </fieldset>
-      <fieldset>
+</fieldset>
+
+<fieldset className="classesContainer">
+        
+      <fieldset className="leftClassDescription">
         <h2>Warlock</h2>
         <p>
           A wielder of magic that is derived from a bargain with an extraplanar
@@ -186,7 +219,7 @@ export const NpcClasses = () => {
           Wisdom & Charisma
         </p>
       </fieldset>
-      <fieldset>
+      <fieldset className="rightClassDescription">
         <h2>Wizard</h2>
         <p>
           A scholarly magic-user capable of manipulating the structures of
@@ -202,6 +235,7 @@ export const NpcClasses = () => {
           <strong>Saves: </strong>
           Intelligence & Wisdom
         </p>
+      </fieldset>
       </fieldset>
       <fieldset>
         <h2>Artificer</h2>
